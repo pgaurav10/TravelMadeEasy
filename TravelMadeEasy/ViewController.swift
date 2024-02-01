a30f599eb4a3113ea776da0be11127784138f49d
Gaurav <gauravpatil24121997@gmail.com>
2024-01-31 22:31:36 -0800

    Initial Commit

--- TravelMadeEasy/ViewController.swift
+++ TravelMadeEasy/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var destinationAddress: UITextField!
+    @IBOutlet weak var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    @IBOutlet weak var getDirections: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
-
+    @IBAction func getDirectionsTapped(_ sender: Any) {
+        
+        getAddress()
+    }
+    
+    func getAddress() {
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(destinationAddress.text!) { placemarks, Error in
+            
+            guard let placemarks = placemarks, let location = placemarks.first?.location
+            else {
+                print("No Location found")
+                return
+            }
+            
+            print(location)
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+    }
 
 }
 
